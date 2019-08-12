9bb7864cdfbf7982c240a48c335ba1d79cc38db7
Vera Weikel <vera.weikel@gmail.com>
2019-08-12 15:14:02 -0700

    Add jquery and return results.

--- js/script.js
+++ js/script.js
@@ -9,21 +9,34 @@ function add(number1, number2)
   {
     return additionResult = number1 + number2;
   };
+return result;
 
-function multiplication(number1, number2) { return number1 * number2; };
+function multiplication(number1, number2) { return resutl = number1 * number2; };
 
 
+function threeTimes(number1, number2, number3)
+{ return result = number1 * number2 * number3; }
 
-const subtraction = alert("Please two numbers to subtract:")
-let subtractionResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 - number2) + ", have a great day!");
 
-const multiplication = alert("Please enter two numbers to multiply:")
-let multiplicationResult = alert("Hi " + inputtedName + ", the difference is " + multiplicationResult((number1 * number2 *) + number3) + ", have a great day!");
-
-const number3;
-const threeTimes = alert("Three numbers multiplied are "  + (number1, number2, number3);
+// const subtraction = alert("Please two numbers to subtract:")
+// let subtractionResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 - number2) + ", have a great day!");
+//
+// const multiplication = alert("Please enter two numbers to multiply:")
+// let multiplicationResult = alert("Hi " + inputtedName + ", the difference is " + multiplicationResult((number1 * number2 *) + number3) + ", have a great day!");
+//
+// const number3;
+// const threeTimes = alert("Three numbers multiplied are "  + (number1, number2, number3);
 
 function add(number1, number2) { return number1 + number2; };
-add(1, 2);
 function add(number1, number2) { return number1 + number2; };
-add(1, 2);
+
+// front end display
+document.ready(function({
+  //jQuery().click(functon()
+  $("p").(functon()
+  {
+
+    $(".subtraction").function(threeTimes){ return result;}
+    alert("Result is: " + result);
+  });
+});
