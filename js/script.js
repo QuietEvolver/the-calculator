b2542a1d20f05f35042c88f16b427dd699ed1c50
Vera Weikel <vera.weikel@gmail.com>
2019-08-12 11:57:02 -0700

    Add js file.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 const inputtedName = prompt("Please enter your name:")
 let greeting = alert("Hi " + inputtedName + ", have a great day!");
+
+const prompt("First number:" + parseInt(number1));
+const prompt("Second number:" + parseInt(number2));
+const subtraction = alert("Please two numbers to subtract:")
+let subtractionResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 - number2) + ", have a great day!");
+
+const multiplication = alert("Please two numbers to multiply:")
+let multiplicationResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 * number2) + ", have a great day!");
+
+const number3;
+const threeTimes = parseInt(number1 * number2 * number3);
