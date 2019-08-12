62a55e88747fcb020d5afc8cdf277e25d3fa1f43
Vera Weikel <vera.weikel@gmail.com>
2019-08-12 13:36:21 -0700

    Add a calculator constants declaration in js.file.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const inputtedName = prompt("Please enter your name:")
 let greeting = alert("Hi " + inputtedName + ", have a great day!");
 
-const prompt("First number:" + parseInt(number1));
-const prompt("Second number:" + parseInt(number2));
+const number1 = prompt(parseInt("First number:"));
+const number2 = prompt(parseInt("Second number:"));
+const number3 = prompt(parseInt("Third number:"));
+
+function add(number1, number2) { return number1 + number2; };
+add(1, 2);
+function multiplication(number1, number2) { return number1 * number2; };
+add(1, 2);
+
 const subtraction = alert("Please two numbers to subtract:")
 let subtractionResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 - number2) + ", have a great day!");
 
-const multiplication = alert("Please two numbers to multiply:")
-let multiplicationResult = alert("Hi inputtedName, the difference is " + subtractionResult(number1 * number2) + ", have a great day!");
+const multiplication = alert("Please enter two numbers to multiply:")
+let multiplicationResult = alert("Hi " + inputtedName + ", the difference is " + multiplicationResult((number1 * number2 *) + number3) + ", have a great day!");
 
 const number3;
-const threeTimes = parseInt(number1 * number2 * number3);
+const threeTimes = alert("Three numbers multiplied are "  + (number1, number2, number3);
+
+function add(number1, number2) { return number1 + number2; };
+add(1, 2);
+function add(number1, number2) { return number1 + number2; };
+add(1, 2);
